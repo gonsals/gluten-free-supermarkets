84792d174eff035059d3d3c5547f8f39f055dbd1
gonsals <marcgonzaleztarrio@gmail.com>
2024-12-11 02:20:38 +0100

    update sdk and fix npm's

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -3,10 +3,10 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: `${process.env.EXPO_PUBLIC_PROJECT_ID}.firebaseapp.com`,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: `${process.env.EXPO_PUBLIC_PROJECT_ID}.appspot.com`,
+    apiKey: process.env.EXPO_PUBLIC_API_KEY,
+    authDomain: `${process.env.EXPO_PUBLIC_PROJECT_ID}.firebaseapp.com`,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    storageBucket: `${process.env.EXPO_PUBLIC_PROJECT_ID}.appspot.com`,
 };
 
 const app = initializeApp(firebaseConfig);
--- hooks/useFetchProduct.ts
+++ hooks/useFetchProduct.ts
@@ -1,49 +1,50 @@
-// hooks/useFetchProduct.ts
 import { useState, useEffect } from "react";
 
 const useFetchProduct = (barcode: string) => {
-  const [product, setProduct] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+    const [product, setProduct] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      if (!barcode) return;
+    useEffect(() => {
+        const fetchProduct = async () => {
+            if (!barcode) return;
 
-      console.log(barcode);
+            console.log(barcode);
 
-      try {
-        setLoading(true);
-        const response = await fetch(
-          `https://world.openfoodfacts.net/api/v2/product/${barcode}`
-        );
-        const data = await response.json();
+            try {
+                setLoading(true);
+                const response = await fetch(
+                    `https://world.openfoodfacts.net/api/v2/product/${barcode}`
+                );
+                const data = await response.json();
 
-        console.log(data);
+                console.log(data);
 
-        if (data.status === 1 && data.product) {
-          setProduct(data.product);
-          setError(null);
-        } else if (data.status === 0) {
-          setError(
-            "Producto no encontrado. Puedes intentar con otro código de barras."
-          );
-          setProduct(null);
-        } else {
-          setError("No se tiene suficiente información sobre el producto.");
-          setProduct(null);
-        }
-      } catch (err) {
-        setError("Error al obtener el producto.");
-      } finally {
-        setLoading(false);
-      }
-    };
+                if (data.status === 1 && data.product) {
+                    setProduct(data.product);
+                    setError(null);
+                } else if (data.status === 0) {
+                    setError(
+                        "Producto no encontrado. Puedes intentar con otro código de barras."
+                    );
+                    setProduct(null);
+                } else {
+                    setError(
+                        "No se tiene suficiente información sobre el producto."
+                    );
+                    setProduct(null);
+                }
+            } catch (err) {
+                setError("Error al obtener el producto.");
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    fetchProduct();
-  }, [barcode]);
+        fetchProduct();
+    }, [barcode]);
 
-  return { product, loading, error, setProduct };
+    return { product, loading, error, setProduct };
 };
 
 export default useFetchProduct;
